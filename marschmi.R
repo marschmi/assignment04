c5ec9966da09a15d9a95469490508a5f18393f07
Marian Schmidt <marschmi@umich.edu>
2014-11-30 22:24:08 -0500

    Deleted extra stuff causing errors.

--- marschmi.R
+++ marschmi.R
@@ -1,50 +1,44 @@
-rm(list=ls())
-In addition, the R file should contain a commented block of text at the top of each function telling anyone how to run each commands. Also, the code should be well commented so that it is clear what each step does. I will test your code by running the following commands:
-  source("marschmi.R")
-paper <- readPaper("mothur.txt")    #I may use a different file
-wordCount(paper, "mothur")          #I may use a different word
-wordPlacement(paper, "mothur")      #I may use a different word
-wordHist(paper)
-wordHist(paper, top=20)
-nextWord(paper, "mothur")           #I may use a different word
-previousWord(paper, "mothur")       #I may use a different word
-surpriseMe(paper)
+rm(list=ls()) #remove all variables from workspace
 
 
-
-#Below this line are my written functions
 ###########################################################################
-
+#Given a file name, create a list variable that contains any necessary information
+    #Input:  A file
+    #Output:  A list
 readPaper <- function(file){
   list <- unlist(scan(file, what = list(""), sep = ""))# Read in the text file.
   list <- gsub("[[:punct:]]", "", list) #remove all of the punctuation
   list <- tolower(list) # change all words to lowercase so "As" and "as" clump together as the same thing.
 }
-paper <- readPaper("mothur.txt")
 #read about the scan function here: 
     #1. http://www.ats.ucla.edu/stat/r/modules/raw_data.htm
     #2. R help
     #3. for gsub:  http://stackoverflow.com/questions/11498157/convert-punctuation-to-space
 #########################
 
-
+#Takes output from readPaper and a word (or a vector of words) and gives the frequency the frequency of the word
+    #Input:  A file and a word
+    #Output:  The frequency of the number
 wordCount <- function(filelist, word){
   sum(filelist == word)  #make a vector of Trues/Falses for every word.  Sum up the matches(trues).
 }
-wordCount(paper, "mothur")
 #Read about functions here:
   #1.  http://stackoverflow.com/questions/1923273/counting-the-number-of-elements-with-the-values-of-x-in-a-vector
 #########################
 
+#Takes a word and output from readPaper and gives the starting character position of that word indexed from the beginning of the paper.
+    #Input:  A file and a word
+    #Output:  A vector of the index of the beginning of each word placement
 wordPlacement <- function(filelist, word){
   which(filelist == word)  #The which function gives the index of a logical object and outputs an array of indices.
 }
-wordPlacement(paper, "mothur")
 #read about the which function here: 
     #1. R help
 #########################
 
-
+#Generates a frequency histogram of the 10 most frequent words in a file, can change the number of words most frequent words
+    #Input:  Filelist and the top X words, default 10
+    #Output:  An image of a histrogram
 wordHist <- function(filelist, top = 10){
   pap <- as.data.frame(table(filelist))  #Make a data frame of all words in the list and its count.
   colnames(pap) <- c("word", "freq")  #Change column names to make them more accurate  
@@ -56,14 +50,15 @@ wordHist <- function(filelist, top = 10){
   labels <- pap$word  #Create vector of names
   text(x, x=x-.5, y=-3.5, labels = labels, srt = 45, pos = 1, xpd = TRUE) #Rotates labels so they look pretty.
 }
-wordHist(paper, top=20)
 #Sources:
   #1. http://www.dummies.com/how-to/content/how-to-sort-data-frames-in-r.html
   #2. http://haotu.wordpress.com/2013/07/09/angle-axis-x-labels-on-r-plot/
   #3. http://stackoverflow.com/questions/20241388/rotate-x-axis-labels-45-degrees-on-grouped-bar-plot-r
 #########################
 
-
+#Given a word, give the frequency of the words that follow it.
+    #Input:  File and a word
+    #Output: Vector of counts
 nextWord <- function(filelist, word){
   something <- which(filelist == word)  #Make a vector of indices of the occurences of the word of interest
   something2 <- something + 1  #Get the index of the word that follows the word of interest.
@@ -73,12 +68,13 @@ nextWord <- function(filelist, word){
     }
   sort(table(test))  #Make a vector with next word and its counts and sort it by increasing abundance
 }
-nextWord(paper, "mothur")
 #Sources:
   #1.  Help with graduate student: Daniel Katz in SNRE.
 #########################
 
-
+#  Given a word, give the freqency of words that preceed it
+    #Input:  File and a word
+    #Output:  Vector of Counts
 previousWord <- function(filelist, word){
   something <- which(filelist == word)  #Make a vector of indices of the occurences of the word of interest
   something2 <- something - 1  #Get the index of the word that preceeds the word of interest.
@@ -88,12 +84,13 @@ previousWord <- function(filelist, word){
   }
   sort(table(test))  #Make a vector with previous word and its counts and sort it by increasing abundance
 }
-previousWord(paper, "mothur")
 #Sources:
-    #1.  Help with graduate student: Daniel Katz in SNRE.
+    #1.  Help from graduate student: Daniel Katz in SNRE.
 #########################
 
-
+# This function takes a readPaper output filelist and outputs a histogram of the frequency of each letter in the alphabet
+    #Input:  File processed through readPaper
+    #Output: Histogram of letter frequency
 surpriseMe <- function(filelist){
   letter_list <- toString(filelist) #Converts the list of words into a list of letters.
   letter_list <- gsub("[[:punct:]]", "", letter_list) #remove all of the punctuation.
@@ -107,7 +104,7 @@ surpriseMe <- function(filelist){
   labels <- toop$oop  #Create vector of names
   text(x, x=x, y=-3.5, labels = labels, srt = 0, pos = 1, xpd = TRUE) #letter labels closer to x-axis.
 }
-surpriseMe(paper)
+
 #########################
 ###########################################################################
 #END
